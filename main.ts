f791d3dfb68f6acfbd8076819cc346640aa5a756
RoniKrasniqi129 <122813938+RoniKrasniqi129@users.noreply.github.com>
2023-01-16 17:17:02 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+    if (input.lightLevel() < 80) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+    if (input.lightLevel() > 80 && input.lightLevel() < 160) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+    if (input.lightLevel() >= 160 && input.lightLevel() < 210) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+    if (input.lightLevel() >= 210) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
